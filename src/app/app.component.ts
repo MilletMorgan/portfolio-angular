9f8d4f57d2465b901acd666b51c60d3f1a995faf
Ozyhcs <millet.morgan5@gmail.com>
2018-10-03 15:48:37 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio-morgan';
+export class AppComponent implements OnInit {
+  
+  ngOnInit() {
+    $(".scrollTo").click(function() {
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+      }, 2000);
+      return false;
+    });
+  }
 }
